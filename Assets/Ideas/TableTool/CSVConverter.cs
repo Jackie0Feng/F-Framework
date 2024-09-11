f554e9f4960b7ef74fe8c32ea76a363aa5476293
jackie feng <2571617419@qq.com>
2024-09-11 17:34:06 +0800

    自动化代码 脚本生成初步

---
+++ Assets/Ideas/TableTool/CSVConverter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FFramework.Ideas
+{
+	/// <summary>
+	/// CSV解析器
+	/// 格式定义
+	/// 每一行用换行符分隔区分
+	/// 基本元素用逗号‘,’分隔区分
+	/// 数组内容用分号‘;’分隔区分
+	/// 表头元数据用变量类型与变量名用‘.’分隔区分
+	/// </summary>
+	public class CSVConverter
+	{
+		/// <summary>
+		/// 序列化CSV文件中的Meta信息
+		/// </summary>
+		/// <param name="csvAsset"></param>
+		/// <returns></returns>
+		public static string[] SerializeCSVMetaString(TextAsset csvAsset)
+		{
+			string[] csvLines = csvAsset.text.Replace("\n", string.Empty).Split("\r");
+			return csvLines[0].Split(',');
+		}
+
+		/// <summary>
+		/// 序列化CSV文件中的Data信息
+		/// </summary>
+		/// <param name="csvData"></param>
+		/// <returns></returns>
+		public static string[][] SerializeCSVDataString(TextAsset csvAsset)
+		{
+			string[] csvLines = csvAsset.text.Replace("\n", string.Empty).Split("\r");
+			string[][] result = new string[csvLines.Length - 1][];
+
+			for (int i = 1; i < csvLines.Length; i++)//跳过表头
+			{
+				result[i - 1] = csvLines[i].Split(",");
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// 将使用“;”分隔的字符串转换为响应的数组
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static T[] ConvertToArray<T>(string value)
+		{
+			string[] strings = value.Split(';');
+
+			T[] result = new T[strings.Length];
+			for (int i = 0; i < result.Length; i++)
+			{
+				result[i] = (T)Convert.ChangeType(strings[i], typeof(T));
+			}
+			return result;
+		}
+	}
+}
---
+++ Assets/Ideas/TableTool/Editor/ScriptGenerater.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace FFramework.Ideas
+{
+	public class ScriptGenerater
+	{
+		private static string GENERATE_SCRIPT_PATH = Application.dataPath + "/Ideas/TableTool/GenerateScripts/";
+
+		private static string EDITOR_PATH = Application.dataPath + "/Ideas/TableTool/Editor/";
+
+		private static string TEMPLATE_IDATABASE_PATH = "Assets/Ideas/TableTool/Editor/Template_IDatabase.txt";
+
+		private static string TEMPLATE_DATABASE_PATH = "Assets/Ideas/TableTool/Editor/Template_Database.txt";
+
+		private static string TEMPLATE_DATABASEMANAGER_PATH = "Assets/Ideas/TableTool/Editor/Template_DatabaseManager.txt";
+
+		private static string CSV_PATH = Application.dataPath + "/Ideas/TableTool/Resources/CsvResources/";
+
+		private static int DATA_ID;
+
+		private static string REGISTER_LIST;
+
+		private static string CONVERT_LIST;
+
+		[MenuItem("TableTool/Generate Script")]
+		public static void GenerateScript()
+		{
+			Initialize();
+			CreateIDatabaseScript();
+			CreateMetaScript();
+		}
+
+		/// <summary>
+		/// 解析CSV文件表头生成类文本
+		/// </summary>
+		private static void CreateMetaScript()
+		{
+			string[] csvPaths = Directory.GetFiles(CSV_PATH, ".csv", SearchOption.AllDirectories);
+			string assetPath = "";
+			TextAsset textAsset = null;
+
+			for (int i = 0; i < csvPaths.Length; i++)
+			{
+				assetPath = "Assets" + csvPaths[i].Replace(Application.dataPath, "").Replace('\\', '/');
+				textAsset = AssetDatabase.LoadAssetAtPath<TextAsset>(assetPath);
+
+				REGISTER_LIST += string.Format("RegisterDataType(new {0}Database());\n", textAsset.name);
+				if (i != csvPaths.Length - 1)
+					REGISTER_LIST += "\t\t\t";
+				CONVERT_LIST += string.Format("CsvToJsonConverter.Convert<{0}Data>(\"{0}\"); \n", textAsset.name);
+				if (i != csvPaths.Length - 1)
+					CONVERT_LIST += "\t\t\t";
+
+				GenerateMetaScript(textAsset);
+			}
+		}
+
+		/// <summary>
+		/// CSV文件表头替换标识符生成类文本
+		/// </summary>
+		/// <param name="textAsset"></param>
+		private static void GenerateMetaScript(TextAsset textAsset)
+		{
+
+		}
+
+		/// <summary>
+		/// 初始化一些数据，创建自动化脚本文件夹
+		/// </summary>
+
+		private static void Initialize()
+		{
+			DATA_ID = 0;
+			REGISTER_LIST = string.Empty;
+			CONVERT_LIST = string.Empty;
+
+			if (Directory.Exists(GENERATE_SCRIPT_PATH))
+			{
+				Directory.Delete(GENERATE_SCRIPT_PATH, true);
+			}
+			Directory.CreateDirectory(GENERATE_SCRIPT_PATH);
+		}
+
+		private static void CreateIDatabaseScript()
+		{
+			string template = GetTemplate(TEMPLATE_IDATABASE_PATH);
+			GenerateScript("IDatabase", template);
+		}
+
+		/// <summary>
+		/// 根据字符串生成脚本文件
+		/// </summary>
+		/// <param name="scriptName"></param>
+		/// <param name="date"></param>
+		/// <exception cref="NotImplementedException"></exception>
+		private static void GenerateScript(string scriptName, string date)
+		{
+			string path = GENERATE_SCRIPT_PATH + scriptName + ".cs";
+			if (File.Exists(path)) { File.Delete(path); }
+
+			StreamWriter sr = File.CreateText(path);
+			sr.WriteLine(date);
+			sr.Close();
+		}
+
+		/// <summary>
+		/// 加载模版txt文件返回字符串
+		/// </summary>
+		/// <param name="path"></param>
+		/// <returns></returns>
+		private static string GetTemplate(string path)
+		{
+			TextAsset textAsset = AssetDatabase.LoadAssetAtPath<TextAsset>(path);
+			return textAsset.text;
+		}
+	}
+}
